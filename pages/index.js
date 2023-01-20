a03a3e5c014f2c1edb1740711568fc9db7bf0c20
Milan Gladiš <hello@milangladis.com>
2023-01-20 21:15:16 +0100

    updated fonts

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home(props) {
         <div className='navigation flex flex-col md:flex-row navigation flex-1 justify-center md:justify-end gap-16 list-none align-middle flex-wrap '>            
           <div className="flex flex-col md:flex-row justify-center items-center gap-8">
             <RotationImage type="content" maxTilt="10">
-              <a href="/cv.pdf" target="_blank" className='flex w-fit h-48 gap-8 px-16 items-center justify-center rounded-full border-2 border-transparent hover:border-grey transition group'>
+              <a href="/cv.pdf" target="_blank" className='flex w-fit h-48 gap-8 px-16 items-center justify-center rounded-full font-semibold border-2 border-transparent hover:border-grey transition group'>
                 <span className="flex gap-8 items-center group-hover:scale-105 transition">
                   Résumé
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -58,11 +58,13 @@ export default function Home(props) {
           </div>
           <div className="hidden md:flex">
             <RotationImage type="content" maxTilt="10">
-              <ButtonLink updatemodal={updatemodal} href="mailto:hello@milangladis.com" className="group border-2 hover:bg-black transition">
-                <span className='group-hover:text-white transition'>Contact me</span>
-                <svg className='group-hover:invert transition' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M8.82843 14.8284L11.5859 19.7467C12.005 20.4942 13.1086 20.4028 13.399 19.5966L17.9363 7.00023C18.2232 6.20399 17.4529 5.43369 16.6566 5.72051L4.06029 10.2579C3.25409 10.5483 3.1627 11.6519 3.91015 12.0709L8.82843 14.8284ZM8.82843 14.8284L10.7441 12.9127" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+              <ButtonLink updatemodal={updatemodal} href="mailto:hello@milangladis.com" className="group border-2 transition">
+                <span className="flex gap-8 items-center group-hover:scale-105 transition">
+                  Contact me
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.82843 14.8284L11.5859 19.7467C12.005 20.4942 13.1086 20.4028 13.399 19.5966L17.9363 7.00023C18.2232 6.20399 17.4529 5.43369 16.6566 5.72051L4.06029 10.2579C3.25409 10.5483 3.1627 11.6519 3.91015 12.0709L8.82843 14.8284ZM8.82843 14.8284L10.7441 12.9127" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
+                </span>
               </ButtonLink>
             </RotationImage>
           </div>
